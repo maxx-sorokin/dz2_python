8b2be3d68b978ac17ec487e9947ce45759587f40
maxx-sorokin <maximka773@mail.ru>
2023-02-04 10:42:22 +0300

    1 ok

--- 1.py
+++ 1.py
@@ -5,9 +5,13 @@
 - 0,56 -> 11
 """
 
-number = input('Введите вещественное число: ')
-summa = 0
-for i in number:
-    if i != '.' and i != ',' and i != '-':
-        summa += int(i)
-print(summa)
+# number = input('Введите вещественное число: ')
+# summa = 0
+# for i in number:
+#     # if i != '.' and i != ',' and i != '-':
+#     #     summa += int(i)
+#     if i.isdigit(): # if i in "0123456789":
+#         summa += int(i)
+# print(summa)
+
+print(sum(map(int, list(str(input('Введите число: ')).replace('.','')))))
